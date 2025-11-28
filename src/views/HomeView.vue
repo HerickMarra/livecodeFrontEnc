fe529069abfa5523c3b6706e4d71b687e5a58412
HerickMarra <herick.marra1@gmail.com>
2025-11-28 15:11:52 -0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+
+const cpf = ref('');
+function enviarCpf(){
+
+  axios.post('http://127.0.0.1:8000/api/cpf',{
+    cpf: cpf.value
+  }).then((response) =>{
+
+  })
+
+}
+
+</script>
+
+<template>
+  <main>
+    <textarea name="" id="" v-model="cpf"></textarea><br>
+    <button @click="enviarCpf()">Enviar Cpf</button>
+  </main>
+</template>
